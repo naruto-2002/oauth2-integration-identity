ea04d1fbe5bc979f7d04d1bbfc280186a08d973f
naruto-2002 <nguyencongvan1002@gmail.com>
2025-07-05 14:01:34 +0700

    Stop tracking configuration.js

--- web-app/src/configurations/configuration.js
+++
@@ -1,5 +0,0 @@
-export const OAuthConfig = {
-  clientId: "379877414704-okgnphpjemlr4fndfptieqd0a88ob64g.apps.googleusercontent.com",
-  redirectUri: "http://localhost:3000/authenticate",
-  authUri: "https://accounts.google.com/o/oauth2/auth",
-};
